416ffabb63c0005bc7f053693d6b34ac3f9f27e1
Josh Lospinoso <josh@lospi.net>
2014-05-11 18:41:16 -0700

    Initial commit

---
+++ lospi-core/src/main/java/net/lospi/Sphinx.java
@@ -0,0 +1,29 @@
+package net.lospi;
+
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+public class Sphinx {
+    private static final Logger logger = LoggerFactory.getLogger(Sphinx.class);
+
+    public static void main(String[] args){
+        Options options = new Options();
+        options.addOption("t", false, "Show the time");
+        CommandLineParser parser = new BasicParser();
+        try {
+            CommandLine commandLine = parser.parse(options, args);
+            if(commandLine.hasOption("t")){
+                logger.info("Current time: " + new Date(System.currentTimeMillis()));
+            } else {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("Sphinx", options);
+            }
+        } catch (ParseException e) {
+            logger.error(e.getMessage());
+            logger.debug(e.toString());
+        }
+    }
+}
